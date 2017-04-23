arter97/caf_msm-5.4

Resolved static analysis issues - set 6
Resolved the following issues reported as part
of static analysis.

Pointer 'tmp1' returned from call to function 'kzalloc' at
line 873 may be NULL and will be dereferenced at line 875.
net/bluetooth/l2cap_core.c +875 | l2cap_conn_start()

Pointer 'hcon' returned from call to function 'hci_connect' at
line 1312 may be NULL and will be dereferenced at line 1320.
net/bluetooth/l2cap_core.c +1320 | l2cap_do_connect()

Suspicious dereference of pointer 'chan' before NULL check at
line 5462.
net/bluetooth/l2cap_core.c +5448 | l2cap_logical_link_complete()

Pointer 'open_worker' returned from call to function 'kzalloc' at
line 7343 may be NULL and will be dereferenced at line 7346.
net/bluetooth/l2cap_core.c +7346 | l2cap_att_channel()

Pointer 'open_worker' checked for NULL at line 7344 will be
dereferenced at line 7346.
net/bluetooth/l2cap_core.c +7346 | l2cap_att_channel()

Pointer 'sk' returned from call to function
'l2cap_find_sock_by_fixed_cid_and_dir' at line 7935 may be NULL and
will be dereferenced at line 7941.
net/bluetooth/l2cap_core.c +7941 | l2cap_queue_acl_data()

Suspicious dereference of pointer 'sk' before NULL check at line 7943.
net/bluetooth/l2cap_core.c +7941 | l2cap_queue_acl_data()

Variable 'sk­->sk_lock.slock' locked on line 7940 was not unlocked.
net/bluetooth/l2cap_core.c +7945 | l2cap_queue_acl_data()

Variable 'sk­>sk_lock.slock' locked on line 7949 was not unlocked.
net/bluetooth/l2cap_core.c +7992 | l2cap_queue_acl_data()

Suspicious dereference of pointer 'wdev' before NULL check at
line 7055
net/wireless/nl80211.c +7033 | nl80211_vendor_cmd()

Change-Id: Ic01b123e7a19e769111eb2bddedd9b63f79f21af
Signed-off-by: Binoy Jayan <bjayan@codeaurora.org>

net/bluetooth/l2cap_core.c
@@ -1,6 +1,6 @@
 /*
    BlueZ - Bluetooth protocol stack for Linux
-   Copyright (c) 2000-2001, 2010-2013 The Linux Foundation. All rights reserved.
+   Copyright (c) 2000-2001, 2010-2016 The Linux Foundation. All rights reserved.
    Copyright (C) 2009-2010 Gustavo F. Padovan <gustavo@padovan.org>
    Copyright (C) 2010 Google Inc.
 
@@ -872,8 +872,10 @@ static void l2cap_conn_start(struct l2cap_conn *conn)
 					L2CAP_CONF_STATE2_DEVICE) {
 				tmp1 = kzalloc(sizeof(struct sock_del_list),
 						GFP_ATOMIC);
-				tmp1->sk = sk;
-				list_add_tail(&tmp1->list, &del.list);
+				if (tmp1 != NULL) {
+					tmp1->sk = sk;
+					list_add_tail(&tmp1->list, &del.list);
+				}
 				bh_unlock_sock(sk);
 				continue;
 			}
@@ -1277,7 +1279,7 @@ int l2cap_do_connect(struct sock *sk)
 {
 	bdaddr_t *src = &bt_sk(sk)->src;
 	bdaddr_t *dst = &bt_sk(sk)->dst;
-	struct l2cap_conn *conn;
+	struct l2cap_conn *conn = NULL;
 	struct hci_conn *hcon;
 	struct hci_dev *hdev;
 	__u8 auth_type;
@@ -1316,19 +1318,21 @@ int l2cap_do_connect(struct sock *sk)
 			err = PTR_ERR(hcon);
 			goto done;
 		}
-
-		conn = l2cap_conn_add(hcon, 0);
-		if (!conn) {
-			hci_conn_put(hcon);
-			err = -ENOMEM;
-			goto done;
+		if (hcon) {
+			conn = l2cap_conn_add(hcon, 0);
+			if (!conn) {
+				hci_conn_put(hcon);
+				err = -ENOMEM;
+				goto done;
+			}
 		}
 	}
 
 	/* Update source addr of the socket */
-	bacpy(src, conn->src);
-
-	l2cap_chan_add(conn, sk);
+	if (conn) {
+		bacpy(src, conn->src);
+		l2cap_chan_add(conn, sk);
+	}
 
 	if ((l2cap_pi(sk)->fixed_channel) ||
 			(l2cap_pi(sk)->dcid == L2CAP_CID_LE_DATA &&
@@ -1350,7 +1354,7 @@ int l2cap_do_connect(struct sock *sk)
 
 		sk->sk_state_change(sk);
 
-		if (hcon->state == BT_CONNECTED) {
+		if (hcon && hcon->state == BT_CONNECTED) {
 			if (sk->sk_type != SOCK_SEQPACKET &&
 				sk->sk_type != SOCK_STREAM) {
 				l2cap_sock_clear_timer(sk);
@@ -5443,6 +5447,10 @@ static void l2cap_logical_link_complete(struct hci_chan *chan, u8 status)
 	struct hci_chan *ampchan;
 	struct hci_conn *ampcon;
 
+	if (chan == NULL) {
+		BT_ERR("%s: status %d, chan %p", __func__, (int) status, chan);
+		return;
+	}
 	BT_DBG("status %d, chan %p, conn %p", (int) status, chan, chan->conn);
 
 	sk = chan->l2cap_sk;
@@ -5459,7 +5467,7 @@ static void l2cap_logical_link_complete(struct hci_chan *chan, u8 status)
 
 	pi = l2cap_pi(sk);
 
-	if ((!status) && (chan != NULL)) {
+	if (!status) {
 		pi->ampcon = chan->conn;
 		pi->ampcon->l2cap_data = pi->conn;
 
@@ -7342,9 +7350,11 @@ static inline int l2cap_att_channel(struct l2cap_conn *conn, __le16 cid,
 		att_chn_params.skb = skb;
 		open_worker = kzalloc(sizeof(*open_worker), GFP_ATOMIC);
 		if (!open_worker)
-			BT_ERR("Out of memory");
-		INIT_WORK(open_worker, l2cap_queue_acl_data);
-		schedule_work(open_worker);
+			BT_ERR("%s: Out of memory", __func__);
+		else {
+			INIT_WORK(open_worker, l2cap_queue_acl_data);
+			schedule_work(open_worker);
+		}
 		goto done;
 	}
 
@@ -7937,16 +7947,22 @@ static void l2cap_queue_acl_data(struct work_struct *worker)
 				att_chn_params.conn->src,
 				att_chn_params.conn->dst,
 				att_chn_params.dir);
-		bh_lock_sock(sk);
-		if (sk->sk_state == BT_CONNECTED) {
-			sock_queue_rcv_skb(sk, att_chn_params.skb);
-			if (sk)
+		if (sk == NULL)
+			BT_ERR("%s: sock not found; cid:%d, dir:%d", __func__,
+				att_chn_params.cid,
+				att_chn_params.dir);
+		else {
+			bh_lock_sock(sk);
+			if (sk->sk_state == BT_CONNECTED) {
+				sock_queue_rcv_skb(sk, att_chn_params.skb);
 				bh_unlock_sock(sk);
-			return;
+				return;
+			}
+			bh_unlock_sock(sk);
 		}
-		bh_unlock_sock(sk);
 	}
-	bh_lock_sock(sk);
+	if (sk)
+		bh_lock_sock(sk);
 
 	if (att_chn_params.skb->data[0] != L2CAP_ATT_INDICATE)
 		goto not_indicate;

daedroza/kernel_sony_apq8064

net/bluetooth/l2cap_core.c
@@ -1,6 +1,6 @@
 /*
    BlueZ - Bluetooth protocol stack for Linux
-   Copyright (c) 2000-2001, 2010-2013 The Linux Foundation. All rights reserved.
+   Copyright (c) 2000-2001, 2010-2016 The Linux Foundation. All rights reserved.
    Copyright (C) 2009-2010 Gustavo F. Padovan <gustavo@padovan.org>
    Copyright (C) 2010 Google Inc.
 
@@ -872,8 +872,10 @@ static void l2cap_conn_start(struct l2cap_conn *conn)
 					L2CAP_CONF_STATE2_DEVICE) {
 				tmp1 = kzalloc(sizeof(struct sock_del_list),
 						GFP_ATOMIC);
-				tmp1->sk = sk;
-				list_add_tail(&tmp1->list, &del.list);
+				if (tmp1 != NULL) {
+					tmp1->sk = sk;
+					list_add_tail(&tmp1->list, &del.list);
+				}
 				bh_unlock_sock(sk);
 				continue;
 			}
@@ -1277,7 +1279,7 @@ int l2cap_do_connect(struct sock *sk)
 {
 	bdaddr_t *src = &bt_sk(sk)->src;
 	bdaddr_t *dst = &bt_sk(sk)->dst;
-	struct l2cap_conn *conn;
+	struct l2cap_conn *conn = NULL;
 	struct hci_conn *hcon;
 	struct hci_dev *hdev;
 	__u8 auth_type;
@@ -1316,19 +1318,21 @@ int l2cap_do_connect(struct sock *sk)
 			err = PTR_ERR(hcon);
 			goto done;
 		}
-
-		conn = l2cap_conn_add(hcon, 0);
-		if (!conn) {
-			hci_conn_put(hcon);
-			err = -ENOMEM;
-			goto done;
+		if (hcon) {
+			conn = l2cap_conn_add(hcon, 0);
+			if (!conn) {
+				hci_conn_put(hcon);
+				err = -ENOMEM;
+				goto done;
+			}
 		}
 	}
 
 	/* Update source addr of the socket */
-	bacpy(src, conn->src);
-
-	l2cap_chan_add(conn, sk);
+	if (conn) {
+		bacpy(src, conn->src);
+		l2cap_chan_add(conn, sk);
+	}
 
 	if ((l2cap_pi(sk)->fixed_channel) ||
 			(l2cap_pi(sk)->dcid == L2CAP_CID_LE_DATA &&
@@ -1350,7 +1354,7 @@ int l2cap_do_connect(struct sock *sk)
 
 		sk->sk_state_change(sk);
 
-		if (hcon->state == BT_CONNECTED) {
+		if (hcon && hcon->state == BT_CONNECTED) {
 			if (sk->sk_type != SOCK_SEQPACKET &&
 				sk->sk_type != SOCK_STREAM) {
 				l2cap_sock_clear_timer(sk);
@@ -5483,7 +5487,13 @@ static void l2cap_logical_link_complete(struct hci_chan *chan, u8 status)
 	struct sock *sk;
 	struct hci_chan *ampchan;
 	struct hci_conn *ampcon;
+
+	if (chan == NULL) {
+		BT_ERR("%s: status %d, chan %pK", __func__, (int) status, chan);
+		return;
+	}
 	BT_DBG("status %d, chan %pK, conn %pK", (int) status, chan, chan->conn);
+
 	sk = chan->l2cap_sk;
 	chan->l2cap_sk = NULL;
 
@@ -5498,7 +5508,7 @@ static void l2cap_logical_link_complete(struct hci_chan *chan, u8 status)
 
 	pi = l2cap_pi(sk);
 
-	if ((!status) && (chan != NULL)) {
+	if (!status) {
 		pi->ampcon = chan->conn;
 		pi->ampcon->l2cap_data = pi->conn;
 
@@ -7381,9 +7391,11 @@ static inline int l2cap_att_channel(struct l2cap_conn *conn, __le16 cid,
 		att_chn_params.skb = skb;
 		open_worker = kzalloc(sizeof(*open_worker), GFP_ATOMIC);
 		if (!open_worker)
-			BT_ERR("Out of memory");
-		INIT_WORK(open_worker, l2cap_queue_acl_data);
-		schedule_work(open_worker);
+			BT_ERR("%s: Out of memory", __func__);
+		else {
+			INIT_WORK(open_worker, l2cap_queue_acl_data);
+			schedule_work(open_worker);
+		}
 		goto done;
 	}
 
@@ -7976,16 +7988,22 @@ static void l2cap_queue_acl_data(struct work_struct *worker)
 				att_chn_params.conn->src,
 				att_chn_params.conn->dst,
 				att_chn_params.dir);
-		bh_lock_sock(sk);
-		if (sk->sk_state == BT_CONNECTED) {
-			sock_queue_rcv_skb(sk, att_chn_params.skb);
-			if (sk)
+		if (sk == NULL)
+			BT_ERR("%s: sock not found; cid:%d, dir:%d", __func__,
+				att_chn_params.cid,
+				att_chn_params.dir);
+		else {
+			bh_lock_sock(sk);
+			if (sk->sk_state == BT_CONNECTED) {
+				sock_queue_rcv_skb(sk, att_chn_params.skb);
 				bh_unlock_sock(sk);
-			return;
+				return;
+			}
+			bh_unlock_sock(sk);
 		}
-		bh_unlock_sock(sk);
 	}
-	bh_lock_sock(sk);
+	if (sk)
+		bh_lock_sock(sk);
 
 	if (att_chn_params.skb->data[0] != L2CAP_ATT_INDICATE)
 		goto not_indicate;
@@ -8123,3 +8141,4 @@ MODULE_PARM_DESC(enable_hs, "Enable A2MP protocol");
 
 module_param(enable_reconfig, bool, 0644);
 MODULE_PARM_DESC(enable_reconfig, "Enable reconfig after initiating AMP move");
+